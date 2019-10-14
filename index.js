fdad75c7531e172ea68d39ff35cf824794c2d06f
Nicolò Rebaioli <niko.reba@gmail.com>
2019-10-14 21:34:00 +0200

    Changed docs server urls and added ssl enforce

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const enforce = require('express-sslify');
 const app = express();
 
 var province = {"padova":"veneto","lodi":"lombardia","lecco":"lombardia","siena":"toscana","oristano":"sardegna","pescara":"abruzzo","milano":"lombardia","pistoia":"toscana","potenza":"basilicata","ragusa":"sicilia","foggia":"puglia","cuneo":"piemonte","matera":"basilicata","l'aquila":"abruzzo","rieti":"lazio","salerno":"campania","napoli":"campania","catania":"sicilia","frosinone":"lazio","cosenza":"calabria","brescia":"lombardia","pesaro e urbino":"marche","cremona":"lombardia","mantova":"lombardia","viterbo":"lazio","vibo valentia":"calabria","ascoli piceno":"marche","terni":"umbria","campobasso":"molise","isernia":"molise","bari":"puglia","caltanissetta":"sicilia","messina":"sicilia","alessandria":"piemonte","bergamo":"lombardia","rovigo":"veneto","verona":"veneto","roma":"lazio","reggio calabria":"calabria","piacenza":"emilia-romagna","sassari":"sardegna","enna":"sicilia","asti":"piemonte","torino":"piemonte","belluno":"veneto","varese":"lombardia","monza e della brianza":"lombardia","novara":"piemonte","agrigento":"sicilia","ancona":"marche","vicenza":"veneto","udine":"friuli-venezia giulia","avellino":"campania","caserta":"campania","biella":"piemonte","benevento":"campania","imperia":"liguria","trento":"trentino-alto adige","pavia":"lombardia","vercelli":"piemonte","savona":"liguria","teramo":"abruzzo","sondrio":"lombardia","parma":"emilia-romagna","como":"lombardia","catanzaro":"calabria","reggio nell'emilia":"emilia-romagna","trapani":"sicilia","bolzano/bozen":"trentino-alto adige","lecce":"puglia","ravenna":"emilia-romagna","palermo":"sicilia","valle d'aosta":"valle d'aosta","fermo":"marche","chieti":"abruzzo","treviso":"veneto","bologna":"emilia-romagna","lucca":"toscana","la spezia":"liguria","pordenone":"friuli-venezia giulia","barletta-andria-trani":"puglia","arezzo":"toscana","venezia":"veneto","verbano-cusio-ossola":"piemonte","macerata":"marche","latina":"lazio","sud sardegna":"sardegna","grosseto":"toscana","genova":"liguria","ferrara":"emilia-romagna","nuoro":"sardegna","cagliari":"sardegna","perugia":"umbria","siracusa":"sicilia","massa-carrara":"toscana","taranto":"puglia","firenze":"toscana","forlì-cesena":"emilia-romagna","modena":"emilia-romagna","rimini":"emilia-romagna","crotone":"calabria","livorno":"toscana","pisa":"toscana","brindisi":"puglia","prato":"toscana","gorizia":"friuli-venezia giulia","trieste":"friuli-venezia giulia"};
@@ -38,6 +39,7 @@ function getComuni(params) {
 }
 
 app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get('/api/comune', function(req, res) {
     res.send(getComuni(req.query));
